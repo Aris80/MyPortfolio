79f73d8bfa3636b50bef3b72e712a78b7002520e
Aris <ahmaadrizkydwiyanto@gmail.com>
2022-07-28 20:35:14 +0700

    first comment

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+window.onscroll = function () {
+  const navbar = document.querySelector('nav')
+  const fixedNav = navbar.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    navbar.classList.add('sticky')
+  }else {
+    navbar.classList.remove('sticky')
+  }
+}
+
+
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', () => {
+   nav.classList.toggle('slide')
+})
+
+//klik di luar hamburger
+window.addEventListener('click', function (e) {
+   if (e.target != menuToggle && e.target != nav) {
+     nav.classList.remove('slide');
+   }
+ });
+
+const scroll = new SmoothScroll('.navbar a[href*="#"]',{
+  speed: 550,
+  speedAsDuration: true
+});
